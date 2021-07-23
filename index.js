215e8fae0fee0fa007f426163f6279fea8ddee6b
Hatice <haticekaraty@gmail.com>
2021-07-22 21:46:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name,email) {
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(jsonObject => document.body.innerHTML = jsonObject.id)
+    .catch(error => {
+        //console.log(error.message);
+        document.body.innerHTML = error.message
+    }); 
+};
+
+//submitData("haley", "haley@email.com")
